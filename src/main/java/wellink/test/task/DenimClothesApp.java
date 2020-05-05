ef7023ced4d9d86a59bd80e0654a810681979fae
Vadim Kazantsev <kazantsev97vs@gmail.com>
2020-05-05 14:53:07 +0700

    DenimClothesApp.java and DenimClothesApp.txt were added

---
+++ src/main/java/wellink/test/task/DenimClothesApp.java
@@ -0,0 +1,12 @@
+package wellink.test.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenimClothesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DenimClothesApp.class, args);
+    }
+}
